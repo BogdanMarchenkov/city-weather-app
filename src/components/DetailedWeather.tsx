93ec5005f61a932d521eebaae26d928845c0800f
BogdanMarchenkov <marchenkov.bogdan@gmail.com>
2023-02-19 11:31:40 +0200

    fix time

--- src/components/DetailedWeather.tsx
+++ src/components/DetailedWeather.tsx
@@ -13,11 +13,11 @@ import humidity from './../images/humidity.svg'
 const Vidget = ({ icon, rate, mark, name }) => {
   return (
     <div style={{ padding: 20 }}>
-      <div style={{ display: 'flex' , alignItems: 'flex-start' }}>
+      <div style={{ display: 'flex', alignItems: 'flex-start' }}>
         <img style={{ width: 40 }} src={icon} alt=''></img>
         <span style={{ fontSize: 24 }}>{Math.round(rate)}</span>
         <span style={{ fontSize: 14 }}>{mark}</span>
-        </div>
+      </div>
       <div style={{ fontSize: 14 }}>{name}</div>
     </div>
   )
@@ -59,15 +59,19 @@ export const DetailedWeather = () => {
     return getGradient(ctx, chartArea)
   }
 
-  const userTimeOffset = -new Date().getTimezoneOffset() * 60
-  const cityTimeOffset = detailed["timezone"]
-  const userTime = new Date().toString().split(' ')[4].split(':')
-  const userTimeArray = (userTime as unknown as number[])
-  const userSecNumber = userTimeArray[0] * 3600 + userTimeArray[1] * 60 + userTimeArray[2] * 1
-  const cityTime3hoursGap = (userSecNumber - userTimeOffset + cityTimeOffset) / 3600 / 3
+  const d = new Date()
+  const localTime = d.getTime()
+  const localOffset = d.getTimezoneOffset() * 60000
+  const utc = localTime + localOffset
+  const cityTimeMillisec = utc + (1000 * detailed["timezone"])
+  const nd = new Date(cityTimeMillisec)
+  const cityTime = nd.toString().split(' ')[4].split(':')
+  const cityTimeArray = (cityTime as unknown as number[])
+  const citySecNumber = cityTimeArray[0] * 3600 + cityTimeArray[1] * 60 + cityTimeArray[2] * 1
+  const cityTime3hoursGap = citySecNumber / 3600 / 3
   const restGapShare = cityTime3hoursGap - Math.floor(cityTime3hoursGap)
   const gapNumber = restGapShare >= 2 / 3 ? Math.ceil(cityTime3hoursGap) : Math.floor(cityTime3hoursGap)
-  const startHourForecast = gapNumber * 3
+  const startHourForecast = gapNumber < 0 ? (8 + gapNumber) * 3 : gapNumber * 3
 
   let timeSlots = []
 
@@ -89,18 +93,18 @@ export const DetailedWeather = () => {
   }
 
   return (
-    <Card sx={{ backgroundColor: '#DCDCDC', minWidth: 310 , borderRadius: 2 }}>
+    <Card sx={{ backgroundColor: '#DCDCDC', minWidth: 310, borderRadius: 2 }}>
       <CardContent>
         <div>
-          {currentCity !== null && <div style={{color: '#19181A'}}>{currentCity as unknown as string}</div>}
-          {detailed !== null && <div style={{ display: "flex", justifyContent: 'space-between', color: '#19181A'}}>
+          {currentCity !== null && <div style={{ color: '#19181A' }}>{currentCity as unknown as string}</div>}
+          {detailed !== null && <div style={{ display: "flex", justifyContent: 'space-between', color: '#19181A' }}>
             <Vidget icon={temp} rate={detailed["temperature"]} mark='°C' name='temperature' />
             <Vidget icon={feels} rate={detailed["feelsLike"]} mark='°C' name='feels like' />
             <Vidget icon={wind} rate={detailed["wind"]} mark='km/h' name='wind speed' />
             <Vidget icon={humidity} rate={detailed["humidity"]} mark='%' name='humidity' />
           </div>}
           <div >
-          {chartData && <LineChart chartData={chartData} />}
+            {chartData && <LineChart chartData={chartData} />}
           </div>
           <Button onClick={backToCards} size="small">back to cards</Button>
         </div>
